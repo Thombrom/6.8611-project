8fb990c0977453f55b9729f5c928b7f9d8a24fb1
Hophin Kibona <hkibona@mit.edu>
2022-11-14 22:23:59 -0500

    extract embeddings from bert

--- models/bert.py
+++ models/bert.py
@@ -1,23 +1,23 @@
 from . import VectorEmbedder
 from transformers import DistilBertTokenizer, DistilBertModel
 import torch
-
 class BertEmbedder(VectorEmbedder):
     def __init__(self):
-        bert_tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
-        tokenizer = lambda text: bert_tokenizer(text, return_tensors='pt')["input_ids"]
-        super(BertEmbedder, self).__init__(tokenizer, 768, 30522)
-        self.model = DistilBertModel.from_pretrained("distilbert-base-uncased")
 
         self.vocab_size = 30522
         self.hidden_size = 768
-        # tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        # bert = BertModel.from_pretrained('bert-base-uncased')
+        bert_tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+        tokenizer = lambda text: bert_tokenizer(text, return_tensors='pt')["input_ids"]
+        super(BertEmbedder, self).__init__(tokenizer, self.hidden_size, self.vocab_size)
+        self.model = DistilBertModel.from_pretrained("distilbert-base-uncased")
 
-        # token_embedding = {token: bert.get_input_embeddings()(torch.tensor(id))  for token, id in tokenizer.get_vocab().items()}
+        self.embeddings = torch.empty((self.vocab_size, self.hidden_size))
+        for word, token in bert_tokenizer.get_vocab().items():
+            print("token", token)
+            self.embeddings[token] = self.model.get_input_embeddings()(torch.tensor(token))
 
-        # print(len(token_embedding))
-        # print(token_embedding['[CLS]'])
         
     def forward(self, x):
         return self.model(input_ids=x, attention_mask=torch.ones(*x.shape)).last_hidden_state[:, 1:][:, :-1]
+
+    
