7704b4b3796335d35cbc6d4aa1d0ddf6fe06dded
Thomas Bro Falkenberg <tbf@mit.edu>
2022-11-26 15:37:52 -0500

    Added training function to transformer

--- models/transformer_vector.py
+++ models/transformer_vector.py
@@ -1,5 +1,6 @@
 from . import VectorEmbedder
 from torch import nn
+from torch.optim import Adam
 
 import torch
 
@@ -54,8 +55,62 @@ class SelfAttentionVectorModel(VectorEmbedder):
         self.vocab_size  = vocab_size
         self.embeddings = nn.Embedding(vocab_size, hidden_size)
         
+        # Setup optimizer
+        self.set_optimizer(Adam(self.parameters(), lr=1e-5))
+        
+        # Setup transformer layers
         self.transformer_layer = TransformerVectorLayer(hidden_size, 64)
         
     def forward(self, x):
         x =  self.embeddings(x)
         return self.transformer_layer(x)
+    
+    def save(self, folder, name):
+        path = os.sep.join([ folder, name ])
+        state = {
+            'state_dict':   self.state_dict(),
+            'optimizer':    self.optimizer.state_dict(),
+            'epochs':       self.num_epochs,
+            'hidden_size':  self.hidden_size,
+            'vocab_size':   self.vocab_size,
+            'tokenizer':    self.tokenizer.save(),
+        }
+        torch.save(state, path)
+        
+    @classmethod
+    def load(cls, tokenizer_cls, path):
+        state = torch.load(path)
+        tokenizer = tokenizer_cls.load(state['tokenizer'])
+        model = cls(tokenizer, state['hidden_size'], state['vocab_size'])
+        model.optimizer.load_state_dict(state['optimizer'])
+        model.num_epochs = state['epochs']
+        model.load_state_dict(state['state_dict'])
+        return model
+    
+    def do_train(self, dataloader, epochs, savepath, epoch_func=lambda model: None):
+        loss_func = nn.NLLLoss(reduction="sum", ignore_index=self.tokenizer.PAD_TOKEN)
+        while self.num_epochs <= epochs:
+            
+            total_loss = 0
+            
+            for tokens, mask_idx, replaced_token in tqdm.tqdm(dataloader, position=0, leave=True):
+                tokens = tokens.to(self.device)
+                mask_idx =  mask_idx.to(self.device)
+                replaced_token = replaced_token.to(self.device)
+
+                output = self.forward(tokens)
+                predictions = self.generator.predict(output)
+                masked_predictions = predictions[torch.arange(len(mask_idx)).unsqueeze(-1), mask_idx.unsqueeze(-1)].squeeze()
+                
+                loss = loss_func(masked_predictions, replaced_token)
+                loss.backward()          
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+                total_loss += loss
+                
+            print(f"Epoch {self.num_epochs}: Total loss {total_loss}")
+            epoch_func(self)
+            self.num_epochs += 1
+            
+            if savepath:
+                self.save(savepath, f"{type(self).__name__}_{self.num_epochs}.tar")
