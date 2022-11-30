58913f93b5e804600bf4d11096257d3db34d3263
Hophin Kibona <hkibona@mit.edu>
2022-11-30 18:45:53 -0500

    use the linear aggregator for

--- models/multi_heads_transformer_matrix.py
+++ models/multi_heads_transformer_matrix.py
@@ -1,37 +1,11 @@
 from . import MatrixEmbedder, SelfAttentionMatrixLayer
 from torch import nn
 from torch.optim import Adam
-import numpy as np
 
-import os
-import tqdm
 import torch
 
-
-class SelfAttentionMatrixLayer(nn.Module):
-    def __init__(self, hidden_shape, maxlen=None):
-        super(SelfAttentionMatrixLayer, self).__init__()
-        self.hidden_shape = hidden_shape
-        
-        # Set up key, query and value projections
-        self.key_proj   = nn.Conv2d(maxlen, maxlen, (3, 3), padding=1, padding_mode='zeros', groups=maxlen)
-        self.query_proj = nn.Conv2d(maxlen, maxlen, (3, 3), padding=1, padding_mode='zeros', groups=maxlen)
-        self.value_proj = nn.Conv2d(maxlen, maxlen, (3, 3), padding=1, padding_mode='zeros', groups=maxlen)
-        
-        self.key_pool   = nn.MaxPool2d(2, stride=2)
-        self.query_pool = nn.MaxPool2d(2, stride=2)
-        self.value_pool = nn.MaxPool2d(2, stride=2)
-        
-    def forward(self, x):
-        key   = self.key_pool(self.key_proj(x))
-        query = self.query_pool(self.query_proj(x))
-        value = self.value_pool(self.value_proj(x))
-        
-        attention = torch.einsum('bijk,bljk->bil', key, query)
-        attention /= (np.prod(self.hidden_shape) / 4)**(1/2)
-        attended_value = torch.einsum('bin,bnjk->bijk', attention, value)
-        return attended_value
-
+import os
+import torch
 class MultiAttentionHeadsTransformerMatrixLayer(nn.Module):
     def __init__(self, hidden_shape, maxlen=None, num_attention_heads=8):
         super(MultiAttentionHeadsTransformerMatrixLayer, self).__init__()
@@ -40,22 +14,23 @@ class MultiAttentionHeadsTransformerMatrixLayer(nn.Module):
         
         # Set up the parts of the transformer
         self.num_attention_heads = num_attention_heads
-        self.self_attention_heads = nn.ModuleList([ SelfAttentionMatrixLayer(hidden_shape, maxlen) for _ in range(self.num_attention_heads)])
+        self.self_attention_head_layers = nn.ModuleList([ SelfAttentionMatrixLayer(hidden_shape, maxlen) for _ in range(self.num_attention_heads)])
         self.self_attention_aggr  = nn.Conv2d(maxlen, 4 * maxlen, (3, 3), padding=1, padding_mode='zeros', groups=maxlen)
-        self.linear_layer = nn.Linear(self.num_attention_heads, 1) #TODO fix layer so it words with any num of attention heads
+        self.linear_layer = nn.Linear(sum(self.hidden_shape)*self.num_attention_heads, sum(self.hidden_shape))
         self.layer_norm1          = nn.LayerNorm(self.hidden_shape)
         self.ffnn                 = nn.Conv2d(maxlen, maxlen, (3, 3), padding=1, padding_mode='zeros')
         self.layer_norm2          = nn.LayerNorm(self.hidden_shape)
 
     def forward(self, x):
-        attention_heads = torch.empty(1, self.maxlen, 8, 8) #TODO what is 8, 8
-        for i in range(self.num_attention_heads):
-          z_i = self.self_attention_heads[i](x)
+        attention_heads = self.self_attention_head_layers[0](x)
+        for i in range(1, self.num_attention_heads):
+          z_i = self.self_attention_head_layers[i](x)
           attention_heads = torch.cat([attention_heads, z_i])
 
-        y = self.self_attention_aggr(attention_heads[1:])
-        # Combine multiple heads(z_i's) to form one z
-        y = self.linear_layer (y) 
+        # Combine multiple heads(z_i's) to one z
+        y = self.linear_layer(attention_heads.permute(1, 2, 3, 0)).permute(3, 0, 1, 2)
+        y = self.self_attention_aggr(y)
+        
         # Transform the shape back up to the original input
         batch_size = x.shape[:-3]
         y = torch.reshape(y, (*batch_size, self.maxlen, *self.hidden_shape))
@@ -68,9 +43,9 @@ class MultiAttentionHeadsTransformerMatrixLayer(nn.Module):
 
 # Essentially follows: https://jalammar.github.io/illustrated-transformer/
 # but abstracted to work for matrices
-class MultiAttentionTransformerMatrixModel(MatrixEmbedder):
+class TransformerMatrixModel(MatrixEmbedder):
     def __init__(self, tokenizer, num_layers, hidden_shape, vocab_size, maxlen):
-        super(MultiAttentionTransformerMatrixModel, self).__init__(tokenizer, hidden_shape, vocab_size, maxlen)
+        super(TransformerMatrixModel, self).__init__(tokenizer, hidden_shape, vocab_size, maxlen)
         self.hidden_shape = hidden_shape
         self.vocab_size  = vocab_size
         self.embeddings = nn.Parameter(torch.randn(vocab_size, *hidden_shape))
@@ -112,7 +87,3 @@ class MultiAttentionTransformerMatrixModel(MatrixEmbedder):
         model.num_epochs = state['epochs']
         model.load_state_dict(state['state_dict'])
         return model
-
-
-# model = MultiAttentionTransformerMatrixModel(tokenizer, 8, (16, 16), tokenizer.vocab_size, 50)
-# epoch_tester(model)
--- models/multi_heads_transformer_vector.py
+++ models/multi_heads_transformer_vector.py
@@ -6,34 +6,36 @@ import os
 import tqdm
 import torch
 
-
 class MultiAttentionHeadsTransformerVectorLayer(nn.Module):
-    def __init__(self, hidden_size, kvq_size = 64, num_attention_heads=4):
+    def __init__(self, hidden_size, kvq_size = 64, num_attention_heads=3):
         super(MultiAttentionHeadsTransformerVectorLayer, self).__init__()
+
+        assert num_attention_heads >= 2, "number of attention heads must >= 2"
         self.hidden_size = hidden_size
+
         self.kvq_size = kvq_size
 
         # Set up the parts of the transformer
         self.num_attention_heads = num_attention_heads
-        self.self_attention       = SelfAttentionVectorLayer(hidden_size, kvq_size)
+        # self.self_attention       = SelfAttentionVectorLayer(hidden_size, kvq_size)
         self.self_attention_head_layers = nn.ModuleList([ SelfAttentionVectorLayer(hidden_size, kvq_size) for _ in range(self.num_attention_heads)])
 
         self.self_attention_aggr = nn.Linear(kvq_size, hidden_size)
-        self.linear_aggr = nn.Linear(kvq_size, 1) #TODO use this instead of summing
+        
+        self.linear_aggr = nn.Linear(32*num_attention_heads, 32) #Where is 32 coming from?
         self.layer_norm1         = nn.LayerNorm(hidden_size)
         self.ffnn                = nn.Linear(hidden_size, hidden_size)
         self.layer_norm2         = nn.LayerNorm(hidden_size)
         
     def forward(self, x):
 
-        attention_heads = torch.empty(1, 1, self.kvq_size)
-        for i in range(self.num_attention_heads):
+        attention_heads = self.self_attention_head_layers[0](x)
+        for i in range(1, self.num_attention_heads):
           z_i = self.self_attention_head_layers[i](x)
           attention_heads = torch.cat([attention_heads, z_i])
-
-        y = attention_heads[1:]
+        
+        y = self.linear_aggr(attention_heads.permute(1, 2, 0)).permute(2, 0, 1)
         y = self.self_attention_aggr(y)
-        y = torch.sum(y, dim=0, keepdims=True) #TODO use self.linear_aggr
         
         y = self.layer_norm1(y + x)
         z = self.ffnn(y)
@@ -84,3 +86,4 @@ class MultiHeadsTransformerVectorModel(VectorEmbedder):
         model.num_epochs = state['epochs']
         model.load_state_dict(state['state_dict'])
         return model
+
