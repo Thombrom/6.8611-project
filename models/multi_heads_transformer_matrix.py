41f9541f237bc05c7143cc489fd929ef971e5335
Hophin Kibona <hkibona@mit.edu>
2022-12-01 15:00:23 -0500

    add batch size parameter to models

--- models/multi_heads_transformer_matrix.py
+++ models/multi_heads_transformer_matrix.py
@@ -1,67 +1,72 @@
-from . import VectorEmbedder, SelfAttentionVectorLayer
+from . import MatrixEmbedder, SelfAttentionMatrixLayer
 from torch import nn
 from torch.optim import Adam
 
-import os
-from tqdm import tqdm
 import torch
 
-class MultiAttentionHeadsTransformerVectorLayer(nn.Module):
-    def __init__(self, hidden_size, kvq_size = 64, num_attention_heads=4, batch_size=32):
-        super(MultiAttentionHeadsTransformerVectorLayer, self).__init__()
-
-        assert num_attention_heads >= 2, "number of attention heads must >= 2"
-        self.hidden_size = hidden_size
+import os
+import torch
+class MultiAttentionHeadsTransformerMatrixLayer(nn.Module):
+    def __init__(self, hidden_shape, maxlen=None, num_attention_heads=8, batch_size=32):
+        super(MultiAttentionHeadsTransformerMatrixLayer, self).__init__()
+        self.hidden_shape = hidden_shape
+        self.maxlen = maxlen
         self.batch_size = batch_size
-        self.kvq_size = kvq_size
 
+        print("Note: Remember to set the correct batch size!!!")
+        
         # Set up the parts of the transformer
         self.num_attention_heads = num_attention_heads
-        self.self_attention_head_layers = nn.ModuleList([ SelfAttentionVectorLayer(hidden_size, kvq_size) for _ in range(self.num_attention_heads)])
+        self.self_attention_head_layers = nn.ModuleList([ SelfAttentionMatrixLayer(hidden_shape, maxlen) for _ in range(self.num_attention_heads)])
+        self.self_attention_aggr  = nn.Conv2d(maxlen, 4 * maxlen, (3, 3), padding=1, padding_mode='zeros', groups=maxlen)
+        self.linear_layer = nn.Linear(batch_size*self.num_attention_heads, batch_size)
+        self.layer_norm1          = nn.LayerNorm(self.hidden_shape)
+        self.ffnn                 = nn.Conv2d(maxlen, maxlen, (3, 3), padding=1, padding_mode='zeros')
+        self.layer_norm2          = nn.LayerNorm(self.hidden_shape)
 
-        self.self_attention_aggr = nn.Linear(kvq_size, hidden_size)
-        
-        self.linear_aggr = nn.Linear(self.batch_size*num_attention_heads, self.batch_size)
-        self.layer_norm1         = nn.LayerNorm(hidden_size)
-        self.ffnn                = nn.Linear(hidden_size, hidden_size)
-        self.layer_norm2         = nn.LayerNorm(hidden_size)
-        
     def forward(self, x):
 
-        if x.shape[0] == 1: #for epoch tester: TODO make metrics send data in batches
-          x = torch.tile(x, (self.batch_size, 1, 1))
+        if x.shape[0] == 1: #for epoch tester
+          x = torch.tile(x, (self.batch_size, 1, 1, 1))
 
         attention_heads = self.self_attention_head_layers[0](x)
         for i in range(1, self.num_attention_heads):
           z_i = self.self_attention_head_layers[i](x)
           attention_heads = torch.cat([attention_heads, z_i])
 
-        y = self.linear_aggr(attention_heads.permute(1, 2, 0)).permute(2, 0, 1)
+        y = self.linear_layer(attention_heads.permute(1, 2, 3, 0)).permute(3, 0, 1, 2)
+
         y = self.self_attention_aggr(y)
         
-        y = self.layer_norm1(y + x)
+        # Transform the shape back up to the original input
+        batch_size = x.shape[:-3]
+        y = torch.reshape(y, (*batch_size, self.maxlen, *self.hidden_shape))
+        
+        # Sum x and y for layer normalization
+        y = self.layer_norm1(torch.add(y, x))
         z = self.ffnn(y)
-        z = self.layer_norm2(z + y)
-
-        return x
+        z = self.layer_norm2(torch.add(y, z))
+        return z
 
 # Essentially follows: https://jalammar.github.io/illustrated-transformer/
-class MultiHeadsTransformerVectorModel(VectorEmbedder):
-    def __init__(self, tokenizer, num_layers, hidden_size, vocab_size):
-        super(MultiHeadsTransformerVectorModel, self).__init__(tokenizer, hidden_size, vocab_size)
-        self.hidden_size = hidden_size
+# but abstracted to work for matrices
+class MultiHeadsTransformerMatrixModel(MatrixEmbedder):
+    def __init__(self, tokenizer, num_layers, hidden_shape, vocab_size, maxlen):
+        super(MultiHeadsTransformerMatrixModel, self).__init__(tokenizer, hidden_shape, vocab_size, maxlen)
+        self.hidden_shape = hidden_shape
         self.vocab_size  = vocab_size
-        self.embeddings = nn.Embedding(vocab_size, hidden_size)
+        self.embeddings = nn.Parameter(torch.randn(vocab_size, *hidden_shape))
         self.num_layers = num_layers
+        self.maxlen = maxlen
         
         # Setup optimizer
         self.set_optimizer(Adam(self.parameters(), lr=1e-3))
         
         # Setup transformer layers
-        self.transformer_layer = nn.ModuleList([ MultiAttentionHeadsTransformerVectorLayer(hidden_size, 64) for _ in range(num_layers) ])
+        self.transformer_layer = nn.ModuleList([ MultiAttentionHeadsTransformerMatrixLayer(hidden_shape, maxlen) for _ in range(num_layers) ])
         
     def forward(self, x):
-        x =  self.embeddings(x)
+        x =  self.embeddings[x]        
         for idx in range(self.num_layers):
             x = self.transformer_layer[idx](x)
         return x
@@ -72,10 +77,11 @@ class MultiHeadsTransformerVectorModel(VectorEmbedder):
             'state_dict':   self.state_dict(),
             'optimizer':    self.optimizer.state_dict(),
             'epochs':       self.num_epochs,
-            'hidden_size':  self.hidden_size,
+            'hidden_shape': self.hidden_shape,
             'vocab_size':   self.vocab_size,
             'tokenizer':    self.tokenizer.save(),
             'num_layers':   self.num_layers,
+            'max_len':      self.maxlen
         }
         torch.save(state, path)
         
@@ -83,10 +89,11 @@ class MultiHeadsTransformerVectorModel(VectorEmbedder):
     def load(cls, tokenizer_cls, path):
         state = torch.load(path)
         tokenizer = tokenizer_cls.load(state['tokenizer'])
-        model = cls(tokenizer, state['num_layers'], state['hidden_size'], state['vocab_size'])
+        model = cls(tokenizer, state['num_layers'], state['hidden_shape'], state['vocab_size'], state['maxlen'])
         model.optimizer.load_state_dict(state['optimizer'])
         model.num_epochs = state['epochs']
         model.load_state_dict(state['state_dict'])
         return model
 
-# model = MultiHeadsTransformerVectorModel(tokenizer, 2, 256, tokenizer.vocab_size, batch_size=32)
+
+# model = MultiHeadsTransformerMatrixModel(tokenizer, 8, (16, 16), tokenizer.vocab_size, 50, batch_size=32)
