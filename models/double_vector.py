736cc1f224126af215574a424cb54f316c45b253
Thomas Bro Falkenberg <tbf@mit.edu>
2022-12-06 23:28:45 -0500

    Changed some stuff for double vector

--- models/double_vector.py
+++ models/double_vector.py
@@ -14,13 +14,17 @@ class DoubleVectorModel(VectorEmbedder):
         self.maxlen = maxlen
         self.vocab_size = vocab_size
 
-        self.embeddings = nn.Parameter(torch.randn(vocab_size, *(1,hidden_size)))
-        self.linear = nn.Linear(maxlen, maxlen, bias=False)
+        self.embeddings = nn.Parameter(torch.randn(vocab_size, hidden_size))
+        self.linear = nn.ModuleList([ nn.Linear(hidden_size * maxlen, hidden_size, bias=False) for _ in range(maxlen) ])
         self.set_optimizer(Adam(self.parameters(), lr=1e-3))
 
     def forward(self, x):
-        x = self.embeddings[x]
-        return self.linear(x)
+        y = self.embeddings[x]
+        z = torch.reshape(y, (*y.shape[:-2], 1, self.maxlen * self.hidden_size))
+        a = torch.cat([ layer(z) for layer in self.linear ], -2)
+        b = torch.reshape(a, (*y.shape[:-2], self.maxlen, self.hidden_size))
+        
+        return b
 
     def save(self, folder, name):
         path = os.sep.join([folder, name])
@@ -46,4 +50,4 @@ class DoubleVectorModel(VectorEmbedder):
         return model
 
 # Usage:
-# model = DoubleMatrixModel(tokenizer, 16, tokenizer.vocab_size, 50)
+# model = DoubleVectorModel(tokenizer, 256, tokenizer.vocab_size, 50)
